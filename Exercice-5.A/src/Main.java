54a6d79dd709484e541ef5a9dbd60fd272a0a5aa
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2023-10-29 00:15:09 +0200

    Semaine 6

--- Exercice-5.A/src/Main.java
+++ Exercice-5.A/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Album album = new Album.Builder("The Dark Side of the Moon", "Pink Floyd")
+                .label("Harvest")
+                .producer("Pink Floyd")
+                .pays("UK")
+                .version("Original")
+                .genre("Rock")
+                .yearOfRelease(1973)
+                .remastered(true)
+                .yearOfOriginalRelease(1973)
+                .standardDebit("CD")
+                .subscribedDebit("Vinyl")
+                .build();
+        System.out.println(album);
     }
 }
